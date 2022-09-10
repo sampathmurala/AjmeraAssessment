73abc4f88834e8f978cc311b55d99d64a798fd0f
Sampath murala <sampath.murala@hotmail.co.in>
2022-09-10 19:07:26 +0530

    commit - removed unused using s

--- AjmeraAssessment/Controllers/AuthorController.cs
+++ AjmeraAssessment/Controllers/AuthorController.cs
@@ -3,12 +3,10 @@ using AjmeraAssessment.Models;
 using AjmeraAssessment.ViewModels;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.Extensions.Caching.Memory;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace AjmeraAssessment.Controllers
 {
